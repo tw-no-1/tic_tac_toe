d8fb867f96532f299fe89863277eb8beff2cea60
whoami13579 <liangforcoding@gmail.com>
2024-06-24 01:26:24 +0800

    update Texture.cpp

--- Texture.cpp
+++ Texture.cpp
@@ -11,6 +11,6 @@ sf::Texture Texture::loadTexture(const char *name) {
     return tmp;
 }
 
-sf::Texture Texture::o = Texture::loadTexture("/Users/mba/myCode/tic-tac-toe/Texture/vecteezy_red-circle-png-red-dot-icon_16017076_782.png");
-sf::Texture Texture::x = Texture::loadTexture("/Users/mba/myCode/tic-tac-toe/Texture/vecteezy_red-x-png-free-download_19899989_464.png");
-sf::Texture Texture::board = Texture::loadTexture("/Users/mba/myCode/tic-tac-toe/Texture/Tic-tac-toe.png");
+sf::Texture Texture::o = Texture::loadTexture("/Users/mba/Code/tic-tac-toe/Texture/vecteezy_red-circle-png-red-dot-icon_16017076_782.png");
+sf::Texture Texture::x = Texture::loadTexture("/Users/mba/Code/tic-tac-toe/Texture/vecteezy_red-x-png-free-download_19899989_464.png");
+sf::Texture Texture::board = Texture::loadTexture("/Users/mba/Code/tic-tac-toe/Texture/Tic-tac-toe.png");
